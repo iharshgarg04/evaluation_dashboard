47a1c1ca589d996c27440730e211242b58ad96c7
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-10 15:13:04 +0530

    created navbar and mentor list

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../App.css'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router'
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const mentorData=localStorage.getItem("mentorData");
+  return (
+    <div className='Nav-header'>
+        <div className='nav-mentor'>
+            MentorView
+        </div>
+        {mentorData &&
+          <Button sx={{color:"white",backgroundColor:"grey" , '&:hover':{
+            backgroundColor:"black"
+          }}} variant='contained' onClick={()=>{
+            navigate("/");
+            localStorage.removeItem("mentorData");
+          }}>
+            LogOut
+          </Button>
+        }
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import "./home.css";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { CircularProgress } from "@mui/material";
+
+const Home = () => {
+const [mentors, setMentors] = useState([]);
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate();
+  useEffect(() => {
+    const fetchMentors=async()=>{
+        try{
+            setLoading(true);
+            const response = await axios.get(`http://localhost:4000/mentor/fetchMentors`);
+            // console.log(response);
+            setMentors(response.data.mentors);
+            setLoading(false);
+        }catch(error){
+            console.log("Error while fetching mentors");
+            toast.error("Error while Fetching mentors");
+        }
+    }
+    fetchMentors();
+  },[]);
+
+  const handleMentor = (mentor)=>{
+    localStorage.setItem("mentorData",JSON.stringify(mentor));
+    navigate("/dashboard");
+  }
+  return (
+    <div className="home-container">
+    <div className="home-header">
+        <div className="home-title">Select a Mentor from the list</div>
+        {loading ? <div className="circular-progress" style={{alignItems:"center",height:"100%"}}> <CircularProgress sx={{color:"white"}} /></div> : 
+        (mentors.map((mentor,index)=>(
+            <div key={index} className="mentors-list" onClick={()=>{handleMentor(mentor)}}>
+                <p>{mentor.name}</p>
+            </div>
+        )))
+        }
+    </div>
+    </div>
+)};
+
+export default Home;
