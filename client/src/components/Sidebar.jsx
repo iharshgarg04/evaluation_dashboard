0b51f17a66854d567ca2422601cf58bcde66f392
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-10 16:55:11 +0530

    created mystudents sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import { Backdrop, Button, CircularProgress, Icon, IconButton } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import StudentList from "./StudentList";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar-container'>Sidebar</div>
-  )
-}
+  const [mystudents, setMystudents] = useState([]);
+  const [markstudent, setMarkstudent] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const mentor = JSON.parse(localStorage.getItem("mentorData"));
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${process.env.REACT_APP_DEPLOYMENT_URL}/student/myStudents`,
+          {
+            headers: {
+              mentor: mentor._id,
+            },
+          }
+        );
+        if (response.status === 200) {
+          console.log(response);
+          setMystudents(response.data.student);
+          console.log(response, "hii students");
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchStudents();
+  }, []);
 
-export default Sidebar
+
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        `${process.env.REACT_APP_DEPLOYMENT_URL}/mentor/submit`,
+        {
+          mentorId: mentor._id,
+        }
+      );
+      if (response.status === 200) {
+        console.log(response);
+        setMarkstudent(response.data.studentMarks);
+        toast.success("marks locked successfully");
+        setLoading(false);
+        console.log(response);
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+  };
+  return (
+    <div className="sidebar-container">
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <div className="sidebar-header">
+        <h2>{mentor.name}</h2>
+        <div>
+          <Button
+            sx={{ color: "black", fontSize: "16px", fontWeight: "600" }}
+            onClick={() => {
+              navigate("Allstudents");
+            }}
+          >
+            Add
+          </Button>
+          <Button
+            sx={{ color: "black", fontSize: "16px", fontWeight: "600" }}
+            onClick={() => navigate("view")}
+          >
+            view
+          </Button>
+        </div>
+      </div>
+      <div className="sidebar-header-title">My students</div>
+      {mystudents.length === 0 ? (
+        <p className="no-student-text">
+          No students added yet click on Add button to start grading students
+        </p>
+      ) : (
+        <StudentList mystudents={mystudents} />
+      )}
+      <div
+        style={{
+          flex: "1.3",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#31363F",
+            "&:hover": {
+              backgroundColor: "grey",
+            },
+          }}
+          onClick={()=>{
+        
+            handleSubmit();
+          }}
+          disabled={loading}
+        >
+          Submit grading
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/components/StudentList.jsx
@@ -0,0 +1,97 @@
+import React, { useContext, useState } from "react";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { Backdrop, CircularProgress, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useNavigate } from "react-router";
+import { myContext } from "../pages/dashboard/Dashboard";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
+const StudentList = ({ mystudents }) => {
+  const { student, setStudent } = useContext(myContext);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const mentor = JSON.parse(localStorage.getItem("mentorData"));
+  const handleDelete = async(student) => {
+    try {
+      setLoading(true);
+      const response = await axios.put(
+        `${process.env.REACT_APP_DEPLOYMENT_URL}/mentor/removestudent`,
+        {
+          mentorId: mentor._id,
+          studentId: student._id,
+        }
+      );
+      if (response.status === 200) {
+        toast.success("student removed successfully");
+        setLoading(false);
+      }
+      console.log(response);
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+  };
+  return (
+    <>
+    <Backdrop
+  sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+  open={loading}
+>
+  <CircularProgress color="inherit" />
+</Backdrop>
+    <div className="sidebar-list">
+      {mystudents.map((stu, index) => (
+        <div
+          key={index}
+          className="student-sidebar-list"
+          onClick={() => {
+            setStudent(stu);
+            navigate("marks");
+          }}
+        >
+          {stu.name}
+          <div className="student-sidebar-icons">
+            <IconButton
+              sx={{
+                "&:hover": {
+                  backgroundColor: "white",
+                  color: "black !important",
+                  zIndex:"2"
+                },
+              }}
+              onClick={(e)=>{
+                e.stopPropagation();
+                setStudent(stu);
+                navigate("editOrAdd")
+              }}
+            >
+              <EditNoteIcon />
+            </IconButton>
+            <IconButton
+              sx={{
+                "&:hover": {
+                  backgroundColor: "white",
+                  color: "black !important",
+                  zIndex:"20"
+                },
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+               { mystudents.length>3 ? handleDelete(stu): toast.error("AtLeast 3 stundets are required")}
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        </div>
+      ))}
+    </div>
+  </>
+  );
+};
+
+export default StudentList;
