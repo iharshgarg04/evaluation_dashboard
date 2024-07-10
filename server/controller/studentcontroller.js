d8fb06d78877a9ee9db27ae23fc561819cb45bc5
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-10 12:30:06 +0530

    addMarksOrUpdateApi

--- server/controller/studentcontroller.js
+++ server/controller/studentcontroller.js
@@ -18,3 +18,71 @@ exports.fetchAllStudents = async(req,res)=>{
         console.log("error while fetching students");
     }
   }
+
+  exports.addMarksOrUpdate = async (req, res) => {
+    try {
+      const {
+        studentId,
+        mentorId,
+        viva,
+        ideation,
+        execution,
+        projectManagement,
+        teamWork,
+      } = req.body;
+      if (
+        !studentId ||
+        !viva ||
+        !ideation ||
+        !execution ||
+        !projectManagement ||
+        !teamWork
+      ) {
+        return res.status(400).send("All fields are requires");
+      }
+  
+      const mentor = await Mentor.findById(mentorId);
+      if (!mentor) {
+        return res.status(400).json({
+          success: false,
+          message: "mentor not found",
+        });
+      }
+  
+      if(mentor.locked===true){
+          return res.status(400).json({
+              success:false,
+              message:"You have already submited the evaluation. You can no longer do changes"
+          })
+      }
+  
+      const totalMarks =
+        viva + ideation + execution + projectManagement + teamWork;
+  
+        const existingMarks = await Marks.findOneAndUpdate(
+          { student: studentId },
+          {
+            $set: {
+              viva: viva,
+              ideation: ideation,
+              execution: execution,
+              projectManagement: projectManagement,
+              teamWork: teamWork,
+              totalMarks: totalMarks,
+            },
+          },
+          { new: true, upsert: true }
+        ).populate("student");
+  
+        const stuMarks = await Student.findByIdAndUpdate(studentId,{totalMarks:totalMarks});
+  
+      return res.status(200).json({
+        success: true,
+        message: "Marks are added successfully",
+        marks: existingMarks,
+      });
+    } catch (error) {
+      console.log(error.message);
+      console.log("error while adding marks");
+    }
+  };
